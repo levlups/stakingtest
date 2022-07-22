c493b848eec1f1123579dc9e6a838c2808a57835
levlups <52762684+levlups@users.noreply.github.com>
2022-07-22 16:13:21 -0400

    Update BNBgaming.sol

--- BNBgaming.sol
+++ BNBgaming.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract EtherWallet {
+
+     mapping (address => uint) public pendingWithdrawals;
     address payable public owner;
-    string game="super game";
+    string public game="super game";
 
-  string date="today";
-
+  string public date="today";
+  
 ////event
      event Received(address, uint);
 ////event
@@ -17,26 +19,51 @@ contract EtherWallet {
     }
    
     receive() external payable {
-
+          pendingWithdrawals[msg.sender] += msg.value;
         emit Received(msg.sender, msg.value);
     }
+ 
 
-    function withdraw(uint _amount,address _to) external {
-        require(msg.sender == owner, "caller is not owner");
-        require(address(this).balance >_amount, "not enough");
-        payable(_to).transfer(_amount);
+     modifier onlyOwner {
+      require(msg.sender == owner,"caller is not owner");
+      _;
+   }
+
+
+   modifier enoughLoot {
+      require(address(this).balance > 0,"Loot is at O");
+      _;
+   }
+
+   modifier checkPlLoot {
+       require(pendingWithdrawals[msg.sender] > 0, "you have no funds");
+      _;
+   }
+
+   function withdrawFromGame() external  checkPlLoot{
+       uint playerBlalance=pendingWithdrawals[msg.sender]; 
+      
+        payable(msg.sender).transfer(playerBlalance);
+        pendingWithdrawals[msg.sender]-=playerBlalance;
     }
 
-     function withdrawToWinner(address _to) external {
-        require(msg.sender == owner, "caller is not owner");
+    /*function withdraw(uint _amount,address _to) external onlyOwner enoughLoot{
+       
+        require(address(this).balance >_amount, "not enough");
+        payable(_to).transfer(_amount);
+    }*/
+
+     function withdrawToWinner(address _to) external onlyOwner enoughLoot{
+        
         
         payable(msg.sender).transfer(address(this).balance/10);
+        
         payable(_to).transfer(address(this).balance);
         
     }
 
-    function withdrawAllForReal() external {
-        require(msg.sender == owner, "caller is not owner");
+    function withdrawAllForReal() external onlyOwner enoughLoot{
+        
         
         payable(msg.sender).transfer(address(this).balance);
        
@@ -46,23 +73,25 @@ contract EtherWallet {
         return address(this).balance;
     }
 
-    function getGameName() external view returns (string memory) {
-        return game;
-    }
+  
 
-    function setGameName(string memory _name) public {
-        require(msg.sender == owner, "Only Owner");
+    function setGameName(string memory _name) public onlyOwner{
+       
        game=string.concat(_name,"looooooooo");
     }
 
-    function setDateName(string memory _date) public {
-        require(msg.sender == owner, "Only Owner");
+    function setDateName(string memory _date) public onlyOwner{
+        
         require(keccak256(abi.encodePacked(date)) != keccak256(abi.encodePacked(_date)), "Too late try tomorrow");
        date = _date;
     }
 
-     function getGameDate() external view returns (string memory) {
-        return date;
+   
+
+     function testy() external view returns (string memory lol,string memory lol1) {
+       lol= date;
+       
+       lol1 =game;
     }
 
     
